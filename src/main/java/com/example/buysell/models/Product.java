5558baf3af3218f558cafc58c254f386f73a8ba6
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-02-09 03:29:59 +0530

    Conect DB

--- src/main/java/com/example/buysell/models/Product.java
+++ src/main/java/com/example/buysell/models/Product.java
@@ -1,13 +1,23 @@
 package com.example.buysell.models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Entity
+@Table(name = "products")
 @AllArgsConstructor
+@NoArgsConstructor
+
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
+    @Column(name ="description",columnDefinition = "text")
     private String description;
     private int price;
     private String city;
--- src/main/java/com/example/buysell/services/ProductService.java
+++ src/main/java/com/example/buysell/services/ProductService.java
@@ -1,38 +1,41 @@
 package com.example.buysell.services;
 
 import com.example.buysell.models.Product;
+import com.example.buysell.repository.ProductRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class ProductService {
-    private List<Product> products = new ArrayList<>();
-    private long ID = 0;
+    private final ProductRepository productRepository;
 
-    {
-        products.add(new Product(++ID, "Iphone 12", "Phone", 32000, "Kiev", "Andrey"));
 
-    }
+    public List<Product> listProducts(String title) {
+        if (title != null)  return productRepository.findByTitle(title);
 
-    public List<Product> listProducts() {
-        return products;
+        return productRepository.findAll();
     }
 
     public void saveProduct(Product product) {
-        product.setId(++ID);
-        products.add(product);
+        log.info("Новый продукт сохранен {}", product);
+        productRepository.save(product);
+
+
     }
 
     public void deleteProduct(Long id) {
-        products.removeIf(product -> product.getId().equals(id));
+        productRepository.deleteById(id);
     }
 
     public Product getProductById(Long id) {
-        for (Product product : products) {
-            if (product.getId().equals(id)) return product;
-        }
-        return null;
+       return   productRepository.findById(id).orElse(null);
+
     }
 }
